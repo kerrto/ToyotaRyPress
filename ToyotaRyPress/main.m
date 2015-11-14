ae27572d3d542a07170c955172e820e5a14d211d
Kerry Toonen <kerrytoonen@neva-bassingthwaites-macbook-pro.local>
2015-11-14 13:36:32 +0900

    Initial Commit

---
+++ ToyotaRyPress/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ToyotaRyPress
+//
+//  Created by Kerry Toonen on 2015-11-14.
+//  Copyright © 2015 Kerrto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
